17f424c23867b2b1a3333354aeb0a0b226d27cef
Mordecai Godwin <mogodwin@axamansard.com>
2024-01-17 00:46:22 +0100

    added security definition for swagger

--- ImagineBookStore.Api/Program.cs
+++ ImagineBookStore.Api/Program.cs
@@ -48,6 +48,31 @@ builder.Services.AddSwaggerGen(swagger =>
     string coreXmlFilePath = Path.Combine(AppContext.BaseDirectory, "ImagineBookStoreCore.xml");
     swagger.IncludeXmlComments(coreXmlFilePath, true);
 
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
+    });
+
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+
     swagger.IgnoreObsoleteActions();
     swagger.IgnoreObsoleteProperties();
 });
